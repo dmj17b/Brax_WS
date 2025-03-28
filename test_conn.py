5a364c602bc55ee542814e6ee90e39fec9797dbb
dmj17b <dmj17b@fsu.edu>
2025-03-28 12:26:45 -0400

    Same

--- test_conn.py
+++ test_conn.py
@@ -1,26 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import zmq
+from pyqtgraph.Qt import QtCore, QtGui
+import pyqtgraph as pg
+import time
+import sys
 
-def plot_data():
+
+
+
+
+def plot_data(socket):
     """
     Receive and plot data from ZeroMQ socket.
     """
-    print("Plotting data...")
-    # Set up ZeroMQ context and socket
-    context = zmq.Context()
-    socket = context.socket(zmq.SUB)
-    socket.connect("tcp://localhost:5555")
-    socket.setsockopt_string(zmq.SUBSCRIBE, "")
 
-    # Set up the plot
-    plt.ion()  # Turn on interactive mode
-    
-    fig, ax = plt.subplots()
-    ax.set_xlabel('Time')
-    ax.set_ylabel('Value')
-    ax.set_title('Real-Time Data Stream')
-    data_dict = socket.recv_pyobj()
+
     while True:
         data_dict = socket.recv_pyobj()
         time = data_dict['time']
@@ -34,8 +29,20 @@ def plot_data():
 
 
 def main():
+    # Set up ZeroMQ context and socket
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect("tcp://localhost:5555")
+    socket.setsockopt_string(zmq.SUBSCRIBE, "")
+        # Set up the plot
+    plt.ion()  # Turn on interactive mode
+    global fig, ax
+    fig, ax = plt.subplots()
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Value')
+    ax.set_title('Real-Time Data Stream')
     print("Data plotter started. Waiting for data...")
-    plot_data()
+    plot_data(socket)
 
 if __name__ == '__main__':
     main()
